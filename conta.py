33ae21943cb8b08e6cb9e2a6daaeb8de6ffa72ed
Fernanda Amaral de Souza <fernanda.amaral00@souunit.com.br>
2023-03-10 10:37:47 -0300

    Correção de erros

--- conta.py
+++ conta.py
@@ -1,8 +1,20 @@
+
 class Conta:
 
-     def __int__(self, numero, titular, saldo, limite):
+     def __init__(self, numero, titular, saldo, limite):
            print('Construindo objeto.... {}'.format(self))
            self.numero = numero
            self.titular = titular
            self.saldo = saldo
            self.limite = limite
+
+     def extrato(self):
+           print("Saldo do {} do titular {}".format(self.saldo, self.titular))
+
+
+     def deposita(self, valor):
+           self.saldo += valor
+
+     def sacar(self, valor):
+           self.saldo -= valor
+
