cde10b57e1d5b401ac8b81735ab06e94c69a02aa
Fernanda Amaral de Souza <fernanda.amaral00@souunit.com.br>
2023-03-11 08:32:58 -0300

    __

--- conta.py
+++ conta.py
@@ -3,18 +3,18 @@ class Conta:
 
      def __init__(self, numero, titular, saldo, limite):
            print('Construindo objeto.... {}'.format(self))
-           self.numero = numero
-           self.titular = titular
-           self.saldo = saldo
-           self.limite = limite
+           self.__numero = numero
+           self.__titular = titular
+           self.__saldo = saldo
+           self.__limite = limite
 
      def extrato(self):
-           print("Saldo do {} do titular {}".format(self.saldo, self.titular))
+           print("Saldo do {} do titular {}".format(self.__saldo, self.__titular))
 
 
      def deposita(self, valor):
-           self.saldo += valor
+           self.__saldo += valor
 
      def sacar(self, valor):
-           self.saldo -= valor
+           self.__saldo -= valor
 
